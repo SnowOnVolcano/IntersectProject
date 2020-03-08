fee82a43f4d7951bf5cc1e0b3a8b1e798cc63a4d
SnowOnVolcano <541359373@qq.com>
2020-03-08 22:24:15 +0800

    init two structs: Point & Line

--- IntersectProject/main.cpp
+++ IntersectProject/main.cpp
@@ -1,19 +1,46 @@
 #include <iostream>
 #include <fstream>
+#include <set>
+#include <vector>
 
 using namespace std;
+
+struct Point
+{
+	float x;
+	float y;
+
+	bool operator < (const Point& other) const {
+		return x < other.x;
+	}
+};
+
+struct Line
+{
+	int a;
+	int b;
+	int c;
+};
+
+void initLine(Line& line, int x1, int y1, int x2, int y2) {
+	line.a = y1 - y2;
+	line.b = x2 - x1;
+	line.c = x1 * y2 - x2 * y1;
+}
 
 
 
 int main(int argc, char* argv[]) {
-	std::ifstream fileIn;
-	std::ofstream fileOut;
-	for (int i = 0; i < argc; i++) {
-		if (strncmp(argv[i], "-i", 2) == 0) {
+	ifstream fileIn;
+	ofstream fileOut;
+	/*for (int i = 0; i < argc; i++) {
+		if ((string)argv[i] == "-i") {
 			fileIn = ifstream(argv[i + 1]);
 		}
-		else if (strncmp(argv[i], "-o", 2) == 0) {
+		else if ((string)argv[i] == "-o") {
 			fileOut = ofstream(argv[i + 1]);
 		}
-	}
+	}*/
+	
+	return 0;
 }
