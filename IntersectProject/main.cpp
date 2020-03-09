cf5be7ce397b88c80253f056bcc26ee86a23fa85
SnowOnVolcano <541359373@qq.com>
2020-03-09 17:29:41 +0800

    refactor: change the init-func of three structures to constructor

--- IntersectProject/main.cpp
+++ IntersectProject/main.cpp
@@ -11,6 +11,10 @@ struct Point
 	float x;
 	float y;
 
+	Point() :x(), y() {}
+
+	Point(float x, float y) :x(x), y(y) {}
+
 	bool operator < (const Point& other) const {
 		return x < other.x;
 	}
@@ -21,6 +25,17 @@ struct Line
 	int a;
 	int b;
 	int c;
+
+	Line() :a(),b(),c() {}
+
+	Line(int a, int b, int c) :a(a), b(b), c(c) {}
+
+	Line(int x1, int y1, int x2, int y2) :
+		a(y1 - y2),
+		b(x2 - x1),
+		c(x1* y2 - x2 * y1)
+	{}
+
 };
 
 struct Circle
@@ -31,35 +46,35 @@ struct Circle
 	int d;
 	int e;
 	int f;
+
+	Circle() :x(), y(), r(), d(), e(), f(){}
+
+	Circle(int x, int y, int r) :
+		x(x), 
+		y(y), 
+		r(r), 
+		d(-2 * x), 
+		e(-2 * y), 
+		f((int)(pow(x, 2) + pow(y, 2) - pow(r, 2))) 
+	{}
+
 };
 
-void initLine(Line& line, int x1, int y1, int x2, int y2) {
-	line.a = y1 - y2;
-	line.b = x2 - x1;
-	line.c = x1 * y2 - x2 * y1;
-}
-
-void initCircle(Circle& circle, int x, int y, int r) {
-	circle.x = x;
-	circle.y = y;
-	circle.r = r;
-	circle.d = -2 * x;
-	circle.e = -2 * y;
-	circle.f = pow(x, 2) + pow(y, 2) - pow(r, 2);
-}
 
 void calLineLineIst(set<Point> &points, Line& line1, Line& line2) {
 	int D;
-	Point point;
 
 	D = line1.a * line2.b - line2.a * line1.b;
+	
 	switch (D)
 	{
 	case 0:
 		break;
 	default:
-		point.x = (line1.b * line2.c - line2.b * line1.c) / (float)D;
-		point.y = (line1.b * line2.c - line2.b * line1.c) / (float)D;
+		Point point = { 
+			(line1.b * line2.c - line2.b * line1.c) / (float)D, 
+			(line2.a * line1.c - line1.a * line2.c) / (float)D 
+		};
 		points.insert(point);
 		break;
 	}
@@ -68,23 +83,25 @@ void calLineLineIst(set<Point> &points, Line& line1, Line& line2) {
 void calLineCircleIst(set<Point>& points, Line& line, Circle& circle) {
 	int intercept;
 
-	intercept = pow(circle.r, 2) * (pow(line.a, 2) + pow(line.b, 2)) - pow(line.a * circle.x + line.b * circle.y + line.c, 2);
+	intercept = (int)(pow(circle.r, 2) * (pow(line.a, 2) + pow(line.b, 2)) - pow(line.a * circle.x + line.b * circle.y + line.c, 2));
 
 	if (intercept < 0) {
 		return;
 	}
 
-	Line tLine;
+	Line tLine = {
+		line.b,
+		-line.a,
+		line.a * circle.y - line.b * circle.x
+	};
+
 	int D;
-	Point tPoint;
-
-	tLine.a = line.b;
-	tLine.b = -line.a;
-	tLine.c = line.a * circle.y - line.b * circle.x;
-
 	D = tLine.a * line.b - line.a * tLine.b;
-	tPoint.x = (tLine.b * line.c - line.b * tLine.c) / (float)D;
-	tPoint.y = (tLine.b * line.c - line.b * tLine.c) / (float)D;
+
+	Point tPoint = {
+		(tLine.b * line.c - line.b * tLine.c) / (float)D,
+		(line.a * tLine.c - tLine.a* + line.c) / (float)D
+	};
 	
 	switch (intercept)
 	{
@@ -92,16 +109,22 @@ void calLineCircleIst(set<Point>& points, Line& line, Circle& circle) {
 		points.insert(tPoint);
 		break;
 	default:
-		Point e; 
-		Point ist1;
-		Point ist2;
-		e.x = line.b / sqrt(pow(line.a, 2) + pow(line.b, 2));
-		e.y = -line.a / sqrt(pow(line.a, 2) + pow(line.b, 2));
-		intercept = sqrt(intercept / (pow(line.a, 2) + pow(line.b, 2)));
-		ist1.x = tPoint.x + e.x * intercept;
-		ist1.y = tPoint.y + e.y * intercept;
-		ist2.x = tPoint.x - e.x * intercept;
-		ist2.y = tPoint.y - e.y * intercept;
+		float vecX;
+		float vecY;
+		float offset;
+
+		vecX = (float)(line.b / sqrt(pow(line.a, 2) + pow(line.b, 2)));
+		vecY = (float)(-line.a / sqrt(pow(line.a, 2) + pow(line.b, 2)));
+		offset = (float)sqrt(intercept / (pow(line.a, 2) + pow(line.b, 2)));
+
+		Point ist1 = {
+			tPoint.x + vecX * offset,
+			tPoint.y + vecY * offset
+		};
+		Point ist2 = {
+			tPoint.x - vecX * offset,
+			tPoint.y - vecY * offset
+		};
 		points.insert(ist1);
 		points.insert(ist2);
 		break;
@@ -113,18 +136,20 @@ void calCircleCircleIst(set<Point>& points, Circle& circle1, Circle& circle2) {
 	int radiusDiff;
 	int centerDis;
 	
-	radiusSum = pow(circle1.r + circle2.r, 2);
-	radiusDiff = pow(circle1.r - circle2.r, 2);
-	centerDis = pow(circle1.x - circle2.x, 2) + pow(circle1.y - circle2.y, 2);
+	radiusSum = (int)pow(circle1.r + circle2.r, 2);
+	radiusDiff = (int)pow(circle1.r - circle2.r, 2);
+	centerDis = (int)(pow(circle1.x - circle2.x, 2) + pow(circle1.y - circle2.y, 2));
 	
 	if (centerDis > radiusSum || centerDis < radiusDiff) {
 		return;
 	}
 
-	Line line;
-	line.a = circle1.d - circle2.d;
-	line.b = circle1.e - circle2.e;
-	line.c = circle1.f - circle2.f;
+	Line line = {
+		circle1.d - circle2.d, 
+		circle1.e - circle2.e,
+		circle1.f - circle2.f
+	};
+	
 	calLineCircleIst(points, line, circle1);
 }
 
@@ -151,6 +176,9 @@ int main(int argc, char* argv[]) {
 	vector<Circle> circles;
 	set<Point> points;
 
+	Line line;
+	Circle circle;
+	
 	fileIn >> N;
 
 	for (int i = 0; i < N; i++) {
@@ -158,9 +186,8 @@ int main(int argc, char* argv[]) {
 		switch (type)
 		{
 		case 'L':
-			Line line;
 			fileIn >> x1 >> y1 >> x2 >> y2;
-			initLine(line, x1, y1, x2, y2);
+			line = Line(x1, y1, x2, y2);
 			for (Line it : lines) {
 				calLineLineIst(points, line, it);
 			}
@@ -170,9 +197,8 @@ int main(int argc, char* argv[]) {
 			lines.emplace_back(line);
 			break;
 		case 'C':
-			Circle circle;
 			fileIn >> x >> y >> r;
-			initCircle(circle, x, y, r);
+			circle = Circle(x, y, r);
 			for (Line it : lines) {
 				calLineCircleIst(points, it, circle);
 			}
